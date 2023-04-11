237186432471a1c7bdfcdc18cd1419b9542beee1
Corvin Schapöhler <scc9le@bosch.com>
2023-04-11 17:09:35 +0200

    Fix node_modules
    ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    Fix gitignore

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 
-
-import MainPage from './components/MainPage.vue'
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App)
 
