18de0b06ac99e84f02f789f657cfebdeedb5b9be
Etone <co.schapoehler@gmail.com>
2023-04-12 18:39:27 +0200

    Add localstorage and exercise wireframe (#2)
    * Add localstorage and exercise wireframe
    
    * Refine cards and add headers
    
    * Style and theme card
    
    * Add reset of local storage
    
    ---------
    
    Co-authored-by: Corvin Schapöhler <scc9le@bosch.com>

---
+++ src/components/ExerciseCard.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { useStorage } from '@vueuse/core'
+import { inject, onMounted } from 'vue';
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
+import Button from 'primevue/button'
+import ExerciseDetails from './ExerciseDetails.vue'
+
+const exercises = inject('workout-exercises')
+
+const levels = useStorage('levels', {
+    Bridges: 1,
+    Pullups: 1,
+    Pushups: 1,
+    Squats: 1,
+    Twists: 1,
+    "Leg Raises": 1,
+})
+
+defineProps(['workout'])
+
+const completeLevel = (workout) => {
+    if(levels.value[workout] >= exercises[workout].length) {
+        alert("Cant increase level, you are at the top already")
+    } else {
+        levels.value[workout] += 1
+    }
+}
+
+</script>
+
+<template>
+    <Card>
+        <template #header>
+            <video controls autoplay muted loop width="320" height="240">
+                <source src="../assets/loops/glute-bridge.mp4" type="video/mp4" />
+            </video>
+        </template>
+        <template #title>{{ workout }} (current Level: {{ levels[workout] }})</template>
+        <template #content>
+            <ExerciseDetails :workout="exercises[workout][levels[workout] - 1]" />
+        </template>
+        <template #footer>
+            <Button icon="pi pi-plus" label="Complete Level" @click="completeLevel(workout)" />
+        </template>
+    </Card>
+    <Divider />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ExerciseDetails.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Divider from 'primevue/divider'
+
+defineProps(['workout'])
+</script>
+
+<template>
+    <div class="flex-container">
+        <div class="flex-child">
+            <p>{{ workout.name }}</p>
+            <p><a :href="workout.link">Instructions</a></p>
+            <p>Goal: {{ workout.sets }}</p>
+        </div>
+        <div class="flex-child">
+            <p>Form Hints</p>
+            <ul>
+                <li v-for="hint in workout.form">
+                    {{ hint }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.flex-container {
+    display: flex;
+}
+
+.flex-child {
+    flex: 1;
+}
+</style>
