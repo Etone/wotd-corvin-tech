29b73bfa0c1119b02a45355fb1f0a4d9ea120d92
Corvin Schapöhler <scc9le@bosch.com>
2023-04-13 11:21:58 +0200

    Autoefresh Video when changing Level

--- src/components/ExerciseVideo.vue
+++ src/components/ExerciseVideo.vue
@@ -1,10 +1,17 @@
 <script setup>
-defineProps(['src'])
+import { watch, ref } from 'vue';
+
+const props = defineProps(['src'])
+const video = ref('video')
+
+watch(() => props.src, () => {
+    video.value.load()
+})
 </script>
 
 <template>
     <div class="video-mask">
-        <video controls autoplay muted loop width="320" height="240">
+        <video controls autoplay muted loop width="320" height="240" ref="video">
             <source :src="src" type="video/mp4" />
         </video>
     </div>
